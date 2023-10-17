a5fa939ec89b920641d79684fc749471e39a42a4
jan <jasiekj1@gmail.com>
2023-10-17 17:35:33 +0100

    weekly trends plot

--- weekly_eu_trends_plot.py
+++ weekly_eu_trends_plot.py
@@ -25,14 +25,19 @@ for day in days:
 # Creating plot
 mean_day = df.groupby(["Metabolite"])[days_norm].mean()
 
-
 mean_day=mean_day.reset_index()
 mean_day= mean_day.rename(columns={col_name:col_name[:-5] for col_name in mean_day.columns[1:]})
 mean_day = mean_day.rename(columns={"Metabolite": "Drug"})
 mean_day=mean_day.melt(id_vars="Drug", value_vars=mean_day.columns[1:], value_name="Normalized drug consumption", var_name="Day of the week")
 fig = px.line(mean_day, x="Day of the week", y="Normalized drug consumption", color='Drug', width=1000, height=630)
 fig.update_layout(
-    title_text='EU drug consumption over the week in 2022 <br><sup>By analysing traces of metabolites in wastewater of 86 European cities </sup>',
+     title=dict(
+        text='EU drug consumption over the week in 2022 <br><sup>By analysing traces of metabolites in wastewater of 86 European cities </sup>',
+        x=0.5,
+        y=0.95,
+        font=dict(
+            size=26,
+        )),
     annotations = [dict(
         x=1.2,
         y=-0.15,    #Trying a negative number makes the caption disappear - I'd like the caption to be below the map
@@ -45,4 +50,4 @@ fig.update_layout(
 )
 
 fig.show()
-fig.write_html("plot_eu_drugs_weekly_trends.html")
+fig.write_html("pplot_weekly_trends.html")
