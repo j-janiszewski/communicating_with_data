d078b007848fa1509ace481db73217050ec12938
naceee <nace.sever@gmail.com>
2023-12-09 13:57:03 +0100

    plot managers

--- final_project/plot_managers.py
+++ final_project/plot_managers.py
@@ -3,18 +3,16 @@ import plotly.graph_objects as go
 from PIL import Image
 
 
-def main():
+def plot_best_managers():
     df = pd.read_csv('data/manager_value_increase_all.csv')
     managers_list = [
         'Pep Guardiola',
-        'Mauricio Pochettino',
+        'Carlo Ancelotti',
         'Unai Emery',
-        'Erik ten Hag',
         'Arsène Wenger',
-        'Jürgen Klopp',
+        # 'Jürgen Klopp',
         'José Mourinho',
         'Frank de Boer'
-        # 'Philippe Clement'
     ]
     df = df[df['manager'].isin(managers_list)]
     df = df.sort_values(by='value_increase', ascending=False)
@@ -53,12 +51,23 @@ def main():
         # white background
         plot_bgcolor='rgba(0,0,0,0)',
         # title
-        title='Mourinho is a good manager, but ... </sup>',
+        title="Wait for the right moment to join the superstar manager<br>"
+              "<sup>Comparing three managers with highest value increase, "
+              "with three of the most famous managers</sup>",
         title_font_size=30,
         title_font_family="Arial",
+        title_font_color="black",
         xaxis_range=[-0.5, len(df['manager']) + 0.5],
         margin=dict(l=30, r=30, t=80, b=50),
     )
+    x_text_positions = [
+        0.35,
+        1.38,
+        2.55,
+        3.45,
+        4.4,
+        5.35,
+    ]
 
     # add annotations for each manager
     annotations = []
@@ -67,23 +76,38 @@ def main():
             if len(manager.split(' ')) == 2 \
             else f"{manager.split(' ')[0]} {manager.split(' ')[1]}<br>{manager.split(' ')[2]}"
         annotations.append(dict(xref='x', yref='y',
-                                x=i + 0.5, y=-0.2,
+                                x=x_text_positions[i], y=-0.4,
                                 text=text,
-                                font=dict(family='Arial', size=16,
-                                          color='black'),
+                                font=dict(family='Arial', size=16, color='black'),
                                 xanchor='center',
                                 showarrow=False))
 
         annotations.append(dict(xref='x', yref='y',
-                                x=i + 0.5, y=df.loc[df['manager'] == manager, 'value_increase'].values[0] * 1.1,
+                                x=x_text_positions[i], y=df.loc[df['manager'] == manager, 'value_increase'].values[0] + 0.2,
                                 text=f"{round(df.loc[df['manager'] == manager, 'value_increase'].values[0], 2)}",
-                                font=dict(family='Arial', size=14,
-                                          color='black'),
+                                font=dict(family='Arial', size=16, color='black'),
                                 showarrow=False))
+
+    # add another annotation for the top 3 managers
+    annotations.append(dict(xref='x', yref='y',
+                            x=1.5, y=-1,
+                            text='Managers with the highest value increase',
+                            font=dict(family='Arial', size=20, color='black'),
+                            xanchor='center',
+                            showarrow=False))
+    # add another annotation for the most famous four managers
+    annotations.append(dict(xref='x', yref='y',
+                            x=4.5, y=-1,
+                            text='Most famous and successful managers',
+                            font=dict(family='Arial', size=20, color='black'),
+                            xanchor='center',
+                            showarrow=False))
+
     fig.update_layout(annotations=annotations)
 
-    fig.show()
+    # fig.show()
+    return fig
 
 
 if __name__ == '__main__':
-    main()
+    plot_best_managers()
